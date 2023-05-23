016ef4f6548ba72c77f3a7531c69a17aa8d3cbd1
opilane <opilane@HP-CZC2349HTR>
2023-05-23 15:31:14 +0300

    Add project files.

---
+++ Crsl/Lisa.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using Xamarin.Essentials;
+using System.Runtime.InteropServices.ComTypes;
+namespace Crsl
+{
+    public partial class Lisa : ContentPage
+    {
+
+        string[] dir;
+        string[] sonad;
+        public Lisa(string path)
+        {
+            string dir_FilePath = Path.Combine(path, "Riigid_pealinnad.txt");
+            string dir_Text = File.ReadAllText(path);
+            dir = dir_Text.Split(';');
+            int length = dir.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                string rnd = dir[i];
+                sonad = rnd.Split(':');
+                var Page = new ContentPage
+                {
+                    Content = new StackLayout
+                    {
+                        TabIndex = i,
+                        Children =
+                        {
+                            new Label
+                            {
+                                FontSize= 20,
+                                Text = sonad[0],
+                                BackgroundColor = Color.Wheat,
+                                TabIndex= i
+                            }
+                        }
+                    }
+                };
+                Children.Add(Page);
+                var lbl = ((StackLayout)Page.Content).Children[0] as Label;
+                var tap = new TapGestureRecognizer();
+                lbl.GestureRecognizers.Add(tap);
+                tap.Tapped += Tap_Tapped;
+
+            }
+        }
+        bool OnOff = true;
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            if (OnOff)
+            {
+                var lbl = sender as Label;
+                string Tekst_on = dir[lbl.TabIndex];
+                sonad = Tekst_on.Split(':');
+                lbl.Text += " - " + sonad[1];
+                OnOff = false;
+            }
+            else
+            {
+                var lbl = sender as Label;
+                string Tekst_on = dir[lbl.TabIndex];
+                sonad = Tekst_on.Split(':');
+                lbl.Text = sonad[0];
+                OnOff = true;
+            }
+        }
+    }
+}
---
+++ Crsl/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
+namespace Crsl
+{
+    public partial class MainPage : ContentPage
+    {
+        string text = "Raamat:Book;" + "Õun:Apple;" + "Lilled:Flowers;";
+        Button corousel, uus_kart;
+        Editor uus_eestisõna, uus_inglisesõna;
+        Label uus;
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Riigid_pealinnad.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+
+            File.WriteAllText(path, text);
+            StackLayout stack = new StackLayout();
+            corousel = new Button()
+            {
+                Text = "Eesti sõna ja selle tõlge",
+                BackgroundColor = Color.DarkOrange,
+            };
+            uus = new Label()
+            {
+                Text = "Kirjuta eesti sõna ja selle tõlge",
+                BackgroundColor = Color.DarkOrange
+            };
+            uus_eestisõna = new Editor()
+            {
+                Placeholder = "Eesti sõna"
+            };
+            uus_inglisesõna = new Editor()
+            {
+                Placeholder = "Inglise sõna"
+            };
+            uus_kart = new Button()
+            {
+                Text = "Lisa uus kart",
+                BackgroundColor = Color.DarkOrange
+            };
+            corousel.Clicked += Corousel_Clicked;
+            stack.Children.Add(corousel);
+            stack.Children.Add(uus);
+            stack.Children.Add(uus_eestisõna);
+            stack.Children.Add(uus_inglisesõna);
+            stack.Children.Add(uus_kart);
+            uus_kart.Clicked += Uus_kart_Clicked;
+            Content = stack;
+        }
+        string uus_tekst;
+        private void Uus_kart_Clicked(object sender, EventArgs e)
+        {
+            uus_tekst = uus_eestisõna.Text + ":" + uus_inglisesõna.Text + ";";
+            File.AppendAllText(path, uus_tekst);
+            uus_eestisõna.Text = null;
+            uus_inglisesõna.Text = null;
+        }
+
+        private async void Corousel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new Lisa(path)));
+        }
+    }
+}
